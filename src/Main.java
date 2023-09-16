4580ea84edb25eb085e8e6e1ed0d49b6d23937e4
user <yannlefranc330@gmail.com>
2023-09-16 17:42:39 +0200

    javas

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        int day = 2;
+        switch (day) {
+            case 1:
+                System.out.println("Laurent");
+                break;
+            case 2:
+                System.out.println("Aline");
+                break;
+            case 3:
+                System.out.println("patricia");
+                break;
+            case 4:
+                System.out.println("virginie");
+                break;
+            case 5:
+                System.out.println("Thierry");
+                break;
+            case 6:
+                System.out.println("josianne");
+                break;
+            case 7:
+                System.out.println("elodie");
+                break;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
